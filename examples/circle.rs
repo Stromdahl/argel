f348138e6f1165596e73c364807b3af072a6b8c0
Mattias Strömdahl <mattias.stromdahl@gmail.com>
2023-01-30 10:03:21 +0100

    better circle example

--- examples/circle.rs
+++ examples/circle.rs
@@ -1,8 +1,33 @@
+use std::time::Instant;
+
 extern crate argel;
 
 fn main() {
-    let mut canvas = argel::Canvas::new(100, 100);
-    canvas.draw(25,25, argel::circle(25), 0x00FF00);
+    const WIDTH: usize = 800;
+    const HEIGHT: usize = 800;
+    const R: i32 = 50;
+    let mut canvas = argel::Canvas::new(WIDTH, HEIGHT);
+    let mut count = 0;
+    let now = Instant::now();
+    for y in 0..HEIGHT as i32 / R / 2 {
+        for x in 0..WIDTH as i32 / R / 2 {
+            if count % 2 == 0 {
+                canvas.draw(x * R * 2, y * R * 2, argel::circle(R), 0x00FF00);
+            }
+            count += 1;
+        }
+        count += 1;
+    }
+    let elapsed_time = now.elapsed();
+    println!(
+        "Circles done after {}ms",
+        elapsed_time.as_millis()
+    );
     let path = "circle.ppm";
     argel::save_ppm_image(canvas, path).unwrap();
+    let elapsed_time = now.elapsed();
+    println!(
+        "Saving done after {}ms",
+        elapsed_time.as_millis()
+    );
 }
